c945e95eae67f16b1ec2cdb78108dc33c09315a7
Mfundo <mfundontini@gmail.com>
2017-05-17 22:48:34 +0200

    Added admin inlines

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import Post, User
 
 
 # Register your models here.
+class PostAdminInline(admin.StackedInline):
+    model = Post
+    can_delete = False
+    verbose_name = "Post"
+    verbose_name_plural = "Authored Posts"
+    fields = ["title"]
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (PostAdminInline, )
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ["__unicode__", "author", "created"]
     list_display_links = ["__unicode__"]
