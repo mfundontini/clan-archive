2c31de7b11f547f02854a012dad6dca26227f318
Mfundo <mfundontini@gmail.com>
2017-08-13 21:43:57 +0200

    pstgres settings

--- project/blog19/settings.py
+++ project/blog19/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'project.blog19.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "clan-archive",
+        "USER": "clan-archive",
+        "PASSWORD": "clan-archive",
+        "HOST": "127.0.0.1",
+        "PORT": "5432"
     }
 }
 
