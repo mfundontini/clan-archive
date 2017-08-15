eec491468be00b92dd93317bc6131fac91d78410
Mfundo <mfundontini@gmail.com>
2017-08-15 22:52:54 +0200

    using django-pagedown

--- archive/posts/forms.py
+++ archive/posts/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
+from pagedown.widgets import PagedownWidget
+
 from .models import Post
 
 
 class PostCreateForm(forms.ModelForm):
+    body = forms.CharField(widget=PagedownWidget)
 
     class Meta:
         model = Post
