fabd5bd987f0fdc9c5af793a573e252ed1b47644
Mfundo <mfundontini@gmail.com>
2017-08-19 06:22:34 +0200

    added reply forms

--- archive/comments/views.py
+++ archive/comments/views.py
@@ -13,42 +13,33 @@ def reply(request, comment):
     if comment.parent:
         parent = comment.parent
 
-    # form = None
-    #
-    # if request.user.is_authenticated():
-    #     initial = {
-    #         "content_type": instance.content_type,
-    #         "object_id": instance.id,
-    #     }
-    #     form = CreateCommentForm(request.POST or None, initial=initial)
-    #     if form.is_valid():
-    #         form_content_type = form.cleaned_data.get("content_type")
-    #         content_type = ContentType.objects.get(model=form_content_type)
-    #         form_object_id = form.cleaned_data.get("object_id")
-    #         form_comment = form.cleaned_data.get("comment_body")
-    #         comment_id = str(request.POST.get("comment_id"))
-    #         try:
-    #             comment_instance = Comment.objects.get(id=comment_id)
-    #             parent = comment_instance
-    #         except:
-    #             parent = None
-    #
-    #         comment, created = Comment.objects.get_or_create(
-    #             user=request.user,
-    #             content_type=content_type,
-    #             object_id=form_object_id,
-    #             body=form_comment,
-    #             parent=parent,
-    #         )
-    #         return HttpResponseRedirect(comment.get_absolute_url())
-    #
-    # context = {
-    #     "comments": comments,
-    #     "instance": instance,
-    #     "form": form,
-    # }
+    form = None
+
+    if request.user.is_authenticated():
+        initial = {
+            "content_type": comment.content_type,
+            "object_id": comment.object_id,
+        }
+        form = CreateCommentForm(request.POST or None, initial=initial)
+        if form.is_valid():
+            form_content_type = form.cleaned_data.get("content_type")
+            content_type = ContentType.objects.get(model=form_content_type)
+            form_object_id = form.cleaned_data.get("object_id")
+            form_comment = form.cleaned_data.get("comment_body")
+            parent = comment
+
+            comment, created = Comment.objects.get_or_create(
+                user=request.user,
+                content_type=content_type,
+                object_id=form_object_id,
+                body=form_comment,
+                parent=parent,
+            )
+            return HttpResponseRedirect(comment.get_absolute_url())
+
     context = {
         "comment": parent,
+        "form": form,
     }
     return render(request, "replies.html", context)
 
